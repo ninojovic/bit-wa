86a6fe9a8ed0415575aab5569e77fd03b71c8aa4
BIT Student <classroom@bgit.rs>
2018-03-14 17:09:31 +0100

    updated API task

--- API - Task/Client/js/ui.js
+++ API - Task/Client/js/ui.js
@@ -1,10 +1,14 @@
 export const showPosts = (postsArray => {
         postsArray.forEach(({title, intro}) => {
         $(".row").append(`
-            <div class="post">
-                <h4>${title}</h4>
-                <p>${intro}</p>
+        <div class="col s12">
+            <div class="card blue-grey darken-1">
+                <div class="card-content white-text">
+                    <span class="card-title">${title}</span>
+                    <p>${intro}</p>
+                </div>
             </div>
+        </div>
         `)
     })
 })
